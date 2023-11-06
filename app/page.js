e7306e7aea74d9f750228eb835f332d113a2358e
Janio <juniorjanio2016@gmail.com>
2023-11-06 10:05:28 -0300

    Commit - 007 / banner e catergorias pagina principal / 0.2.1

--- app/page.js
+++ app/page.js
@@ -1,10 +1,35 @@
+
 import Loading from './loading'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <>
-      Olá mundo
-    </>
+    <div>
+      <div id={styles.banner}>
+        <h1>MyCommerce</h1>
+        <p>O lugar certo para realizar seus sonhos!</p>
+      </div>
+      <div id={styles.categorias}>
+        <div>
+          <img src="/images/ui/categoria_temporaria.png" alt='image'/>
+          <p>Categoria 1</p>
+        </div>
+        <div>
+          <img src="/images/ui/categoria_temporaria.png" alt='image'/>
+          <p>Categoria 2</p>
+        </div>
+        <div>
+          <img src="/images/ui/categoria_temporaria.png" alt='image'/>
+          <p>Categoria 3</p>
+        </div>
+        <div>
+          <img src="/images/ui/categoria_temporaria.png" alt='image'/>
+          <p>Categoria 4</p>
+        </div>
+      </div>
+      <div id={styles.produtos}>
+        produtos
+      </div>
+    </div>
   )
 }
