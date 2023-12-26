fa312753c3038d38ded6f2228f3706ae288d423d
Janio <juniorjanio2016@gmail.com>
2023-12-26 10:10:26 -0300

    Commit - 035 / componente de categoria do dashboard finalizado / 0.10.3

--- Documentos/Doc_Commits.md
+++ Documentos/Doc_Commits.md
@@ -60,3 +60,4 @@ Abaixo devem ser listados todos os commits realizados (com exceção dos commits
 | 032 | 0.8.3 | Página de produto finalizada. |
 | 033 | 0.9.3 | Página de carrinho finalizada. |
 | 034 | 0.9.3 | Fetchs nas páginas principal e de pesquisa movidos para services. |
+| 035 | 0.10.3 | Componente de categorias no dashboard de admin finalizado. |
--- services/categoryServices.js
+++ services/categoryServices.js
@@ -1,6 +1,6 @@
 import { api, requestConfig } from "@/utils/config"
-
 import { getUser as getUserToken } from "@/utils/userUtils";
+import awsService from "./awsServices";
 
 const getAll = async() => {
   const config = requestConfig("GET", null);
@@ -10,8 +10,62 @@ const getAll = async() => {
   return res;
 }
 
+const createCategory = async(data) => {
+  if(data.image){
+    const image = data.image;
+
+    const randomName = Date.now() + image.name;
+
+    awsService.sendFile(image, "categories", randomName);
+    
+    const imageURL = "https://mycommercetutorial.s3.sa-east-1.amazonaws.com/categories/" + randomName;
+
+    data.image = imageURL;
+  }
+
+  const token = getUserToken().token;
+
+  const config = requestConfig("POST", data, token);
+
+  const res = await fetch(api + "/categories/", config).then((res) => res.json());
+  return res;
+
+}
+
+const updateCategory = async(data) => {
+  if(data.image){
+    const image = data.image;
+
+    const randomName = Date.now() + image.name;
+    awsService.sendFile(image, "categories", randomName);
+    
+    const imageURL = "https://mycommercetutorial.s3.sa-east-1.amazonaws.com/categories/" + randomName;
+
+    data.image = imageURL;
+  }
+
+  const token = getUserToken().token;
+
+  const config = requestConfig("PUT", data, token);
+
+  const res = await fetch(api + "/categories/" + data._id, config).then((res) => res.json());
+  return res;
+}
+
+const deleteCategory = async(category) => {
+  const token = getUserToken().token;
+
+  const config = requestConfig("DELETE", null, token);
+
+  const res = await fetch(api + "/categories/" + category, config).then((res) => res.json());
+  return res;
+}
+
 const categoryService = {
   getAll,
+  createCategory,
+  updateCategory,
+  deleteCategory,
 };
 
 export default categoryService;
